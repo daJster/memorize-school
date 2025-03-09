f5d04c30f1949f219a3e65260d927c5622801149
EL KARCHI Jad <jad.el-karchi@inria.fr>
2025-03-09 15:15:35 +0100

    project init backend

---
+++ hafidh/README.md
@@ -0,0 +1,1 @@
+# Hafidh App Project
---
+++ hafidh/src/main/java/com/aca/hafidh/HafidhApplication.java
@@ -0,0 +1,13 @@
+package com.aca.hafidh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HafidhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HafidhApplication.class, args);
+	}
+
+}
